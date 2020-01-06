9ce9cfa467a91eb8e955299674413d45213840f5
Spiderten <spidertenthiago@gmail.com>
2020-01-06 01:12:42 -0300

    primeira parte do treinamento de banco

---
+++ consultarComJoins.sql
@@ -0,0 +1,8 @@
+-- retorna apenas a relação entre prefeito e cidade
+SELECT * FROM cidades c INNER JOIN prefeitos p on c.id = p.cidade_id;
+
+-- retona o lado da busca de todos os prefeitos
+SELECT * FROM cidades c right JOIN prefeitos p on c.id = p.cidade_id; 
+
+-- retona o lado da busca de todas as cidades
+SELECT * FROM cidades c left JOIN prefeitos p on c.id = p.cidade_id;
