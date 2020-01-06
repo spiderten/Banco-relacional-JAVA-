49184310167086b5c31715987864ee489b8e0a9b
Spiderten <spidertenthiago@gmail.com>
2020-01-06 01:17:44 -0300

    primeira parte do treinamento de banco

--- consultarComJoins.sql
+++ consultarComJoins.sql
@@ -6,3 +6,8 @@ SELECT * FROM cidades c right JOIN prefeitos p on c.id = p.cidade_id;
 
 -- retona o lado da busca de todas as cidades
 SELECT * FROM cidades c left JOIN prefeitos p on c.id = p.cidade_id;
+
+-- simulando full join
+SELECT * FROM cidades c right JOIN prefeitos p on c.id = p.cidade_id
+union
+SELECT * FROM cidades c left JOIN prefeitos p on c.id = p.cidade_id;
